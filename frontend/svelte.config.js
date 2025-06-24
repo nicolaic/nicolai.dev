67573acc9c6b50d5865a6caed18c3ab9e2722d1f
Nicolai <contact@nicolai.dev>
2025-06-24 23:20:31 +0200

    Migrate Wrangler files to JSON, integrate Turbo, and update CI/CD workflow

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,9 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+		pages: "./dist"
+	}),
+  },
 };
 
 export default config;
